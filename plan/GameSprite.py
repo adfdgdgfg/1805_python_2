dcbb34a16f98d1636a56ccaf7002a062c2b06af9
赵庆元 <qingyuan@geekniu.com>
2018-07-16 11:02:35 +0800

    飞机大战

---
+++ plan/GameSprite.py
@@ -0,0 +1,11 @@
+import pygame
+class GameSprite(pygame.sprite.Sprite):#父类 大写
+
+    def __init__(self,image,speed=1):
+        super().__init__()#调用父类方法
+        self.image = pygame.image.load(image)
+        self.rect = self.image.get_rect()
+        self.speed = speed
+
+    def update(self):
+        self.rect.y +=self.speed
