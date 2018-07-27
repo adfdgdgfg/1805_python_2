1d6331a83598863042912bb26205d34417abed73
赵庆元 <qingyuan@geekniu.com>
2018-07-27 15:58:57 +0800

    代码

---
+++ 18day/1.py
@@ -0,0 +1,8 @@
+
+import re
+s = '''<img data-original="https://rpic.douyucdn.cn/appCovers/2016/11/13/1213973_201611131917_small.jpg" src="https://rpic.douyucdn.cn/appCovers/2016/11/13/1213973_201611131917_small.jpg" style="display: inline;">'''
+
+
+ret = re.search(r"(https://.*?\.jpg)",s)
+print(ret.group(1))
+#print(s)
